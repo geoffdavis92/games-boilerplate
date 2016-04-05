4fa53e01b08e46411cdaeebe04cf311a24951549
Geoff Davis <gdavis@archermalmo.com>
2016-04-05 16:06:57 -0500

    update script, add cards.json

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,3 +1,15 @@
 // demo of Random.js
 
 const dice = setDice(6,2)
+const deck = setDeck()
+
+for(let int = 0; int<100;int++) {
+	dice.roll((o,set) => {
+		console.log(o.d1, o.d2)
+		if(o.d1 === 6 && o.d2 === 6) {
+			console.log('CRITICAL HIT!')
+			set('#result',`Roll #${int} => d1: ${o.d1}, d2: ${o.d2}`)
+			return true
+		}
+	})
+}
--- src/js/random.js
+++ src/js/random.js
@@ -3,15 +3,24 @@ class Dice {
 		this.s = dieSides
 		this.n = dieCount
 		this.r = rolls
-		this.dArr = []
-		this.sArr = 0
+		this.set = (selector,value) => {
+			const el = document.querySelector(selector)
+			el.innerText = value
+			return false
+		}
 		return {
-			roll: () => {
+			setup: {
+				dieSides: 	this.s,
+				dieNum: 	this.n,
+				rollCount: 	this.r
+			},
+			roll: (c) => {
 				let a = [],
 					o = {},
 					s = this.s,
 					n = this.n,
 					r = this.r,
+					set = this.set,
 					sum = 0
 				for (let i=0;i<n;i++) {
 					let rn = Math.floor(Math.random() * s + 1)
@@ -25,37 +34,47 @@ class Dice {
 					}
 					return result
 				}
-				console.log(sum())
 				o['sum'] = sum()
+				
+				// Call callback
+				if(c) {
+					c(o,set);
+				}
+
+				// return object
 				return o
-				/*this.dArr = []
-				this.sArr = []
-				let s = this.s,
-					n = this.n,
-					r = this.r,
-					dieArr = [],
-					sum = 0,
-					getRolls = () => {
-						for(let i=0;i<n;i++) {
-							let result = Math.floor(Math.random() * s + 1)
-							dieArr.push(result)
-							sum += result
-						}
-						return {die:dieArr,sum:sum}
-					}
-					for(let i=0;i<r;i++) {
-						let v = getRolls()
-						this.sArr = (v.sum)
-						this.dArr.push(v.die)
-					}
-				return {
-					setup: {dieSides:s,diceCount:n,rollCount:r},
-					sum: this.sArr,
-					dieResults: this.dArr
-				}*/
 			}
 		}
 	}
 }
 
+class Deck {
+	constructor(type="standard",size=52,cards=['a^','a<3','a+','a<>']) {
+		this.type = type
+		this.size = size
+		this.cards = cards
+		this.shuffle = (cards) => {
+			const srcDeck = cards,
+				  shuffledDeck = []
+			for (let i=0;srcDeck.length !== 0;i++) {
+				let selection = Math.floor(Math.random() * srcDeck.length)
+				console.log(selection,shuffledDeck,srcDeck)
+				shuffledDeck.push(srcDeck[selection])
+				console.log("srcDeck pre:", srcDeck,shuffledDeck)
+				srcDeck.splice(srcDeck[selection],1)
+				console.log("srcDeck post:", srcDeck,shuffledDeck)
+			}
+			return shuffledDeck
+		}
+	}
+	deal(nHands,nCards,deck=this.shuffle(this.cards)) {
+		for (let c=0;c<nCards;c++) {
+			for (let h=0;h<nHands;h++) {
+
+			}
+		}
+	}
+} 
+
 const setDice = (s,n,r) => new Dice(s,n,r)
+const setDeck = () => new Deck()
